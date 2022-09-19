6b6456a4b2841b78699a965fb9477011923bef4d
RafaelKeiti <keitiazuma@gmail.com>
2022-09-19 20:17:42 -0300

    feat: Exercicio 14 recebe o nome de uma fruta de acordo com a condição e apresenta uma mensagem definida.

---
+++ Ex14.js
@@ -0,0 +1,20 @@
+function VendaFrutas(fruta) {
+    switch (fruta) {
+        case 'maçã':
+            console.log('Não vendemos esta fruta aqui.')
+            break
+        case 'kiwi':
+            console.log('Estamos com escassez de kiwis.')
+            break
+        case 'melancia':
+            console.log('Aqui está, são 3 reais o quilo.')
+            break
+        default:
+            console.log('Opcao Invalida')
+    }
+}
+
+VendaFrutas('maçã')
+VendaFrutas('kiwi')
+VendaFrutas('melancia')
+VendaFrutas('jaca')
