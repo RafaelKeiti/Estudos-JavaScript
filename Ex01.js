14975e677dd0ff7331277499328b1ae3c72ecd9a
RafaelKeiti <keitiazuma@gmail.com>
2022-09-19 18:46:03 -0300

    feat: Exercicio 01 funcionalidade de uma calculadora.

---
+++ Ex01.js
@@ -0,0 +1,7 @@
+function Resolve(valor1, valor2) {
+    console.log(valor1 + valor2)
+    console.log(valor1 - valor2)
+    console.log(valor1 * valor2)
+    console.log(valor1 / valor2)
+}
+Resolve(5, 5)
